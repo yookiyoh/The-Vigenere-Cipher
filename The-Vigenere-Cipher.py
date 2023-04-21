4b4a8e9738163be9e56058b9b1b3f0cc297fcb4a
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-21 09:47:36 +0800

    Define the character substitute dictionary for ciphertext sourcing

--- The-Vigenere-Cipher.py
+++ The-Vigenere-Cipher.py
@@ -8,3 +8,8 @@ import random
 # Define the font for Pygame
 pygame.font.init()
 font = pygame.font.SysFont("monospace", 48)
+
+# Define the character substitute dictionary
+sub_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
+            'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18,
+            'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
