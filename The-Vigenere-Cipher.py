a52014d3259f4aaf39cc6eb944e23c7e2790c309
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-21 09:26:22 +0800

    Pseudocode

--- The-Vigenere-Cipher.py
+++ The-Vigenere-Cipher.py
@@ -1,3 +1,23 @@
 # Ralph Lorenz I. Codilan
 # BSCpE 1-5
 # Object-Oriented Programming - Assignment 2 - Problem 3
+
+# Start Code
+
+# Define the character substitute dicitionary
+
+# Get inputs
+
+# Encrypt plain text using the Vigenere Cipher 
+
+# Convert the letter to the corresponding number value
+
+# Calculate the ciphertext number value
+
+# Convert the ciphertext number back to its corresponding letter
+
+# Update keyword index 
+
+# Create a Pygame window and display (print) the ciphertext
+
+# Wait for the user to close the window 
