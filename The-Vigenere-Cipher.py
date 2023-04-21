97894e8a4bf1b83e549f4639f7424c66f4996bb9
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-21 09:50:45 +0800

    Get plaintext and keyword from user input

--- The-Vigenere-Cipher.py
+++ The-Vigenere-Cipher.py
@@ -13,3 +13,7 @@ font = pygame.font.SysFont("monospace", 48)
 sub_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
             'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18,
             'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
+
+# Get the plaintext and keyword from user input
+plaintext = input("Enter a message (all uppercase, no spaces): ")
+keyword = input("Enter a keyword (all uppercase): ")
