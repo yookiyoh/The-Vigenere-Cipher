44f16e623ac179769ff7586290b824360c5f2ed5
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-21 09:32:37 +0800

    Additional pseudocodes

--- The-Vigenere-Cipher.py
+++ The-Vigenere-Cipher.py
@@ -5,6 +5,32 @@
 # Start Code
 
 # Define the character substitute dictionary
+   # 'A' corresponds to the '0' number value
+   # 'B' corresponds to the '1' number value
+   # 'C' corresponds to the '2' number value
+   # 'D' corresponds to the '3' number value
+   # 'E' corresponds to the '4' number value
+   # 'F' corresponds to the '5' number value
+   # 'G' corresponds to the '6' number value
+   # 'H' corresponds to the '7' number value
+   # 'I' corresponds to the '8' number value
+   # 'J' corresponds to the '9' number value
+   # 'K' corresponds to the '10' number value
+   # 'L' corresponds to the '11' number value
+   # 'M' corresponds to the '12' number value
+   # 'N' corresponds to the '13' number value
+   # 'O' corresponds to the '14' number value
+   # 'P' corresponds to the '15' number value
+   # 'Q' corresponds to the '16' number value
+   # 'R' corresponds to the '17' number value
+   # 'S' corresponds to the '18' number value
+   # 'T' corresponds to the '19' number value
+   # 'U' corresponds to the '20' number value
+   # 'V' corresponds to the '21' number value
+   # 'W' corresponds to the '22' number value
+   # 'X' corresponds to the '23' number value
+   # 'Y' corresponds to the '24' number value
+   # 'Z' corresponds to the '25' number value
 
 # Get inputs
 
